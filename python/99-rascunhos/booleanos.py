2f6729473b293e8d43b34430f5e505f1ed3f4b80
Salumao Barbosa da Costa <salubcosta@gmail.com>
2025-09-14 17:42:49 -0300

    Rascunho enquanto assistia aula de booleanos

---
+++ python/99-rascunhos/booleanos.py
@@ -0,0 +1,29 @@
+# Verdadeira
+True
+# Falso
+False
+
+if True:
+    print("Bloco IF vai ser executado")
+else:
+    print("Bloco ELSE não será executado")
+
+# Operadores lógicos: and e or
+
+if True and True:
+    print("Bloco será executado")
+
+if True and False:
+    print("Bloco não será executado")
+
+if False and False:
+    print("Bloco não será executado")
+
+if True or True:
+    print("Bloco será executado")
+
+if True or False:
+    print("Bloco será executado")
+
+if False or False:
+    print("Bloco não será executado")
