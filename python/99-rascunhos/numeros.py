57afe5a206d31599512a4ca6d72462b5c5267c63
Salumao Barbosa da Costa <salubcosta@gmail.com>
2025-09-14 00:35:39 -0300

    Rascunho de operações com número enquanto assistia aula de python

---
+++ python/99-rascunhos/numeros.py
@@ -0,0 +1,26 @@
+numero_inteiro = 4
+print("Inteiro = ", numero_inteiro)
+
+numero_real = 3.14
+print("Real com ponto flutuante = ", numero_real)
+
+print("Tipo da variavel inteiro = ", type(numero_inteiro))
+print("Tipo da variavel real = ", type(numero_real))
+
+# Operações
+a = 1
+b = 1
+
+soma =          a + b
+subtracao =     a - b
+multiplicacao = a * b
+divisao =       a / b
+
+print(a, "+", b, " = ", soma)
+print(a, "-", b, " = ", subtracao)
+print(a, "*", b, " = ", multiplicacao)
+print(a, "/", b, " = ", divisao)
+
+# Módulo
+resto_divisao = 10 % 3
+print(resto_divisao)
