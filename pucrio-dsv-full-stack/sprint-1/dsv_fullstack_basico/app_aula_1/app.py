0b766a4514f43b9acd872084da8193cf881a696f
Salumao Barbosa da Costa <salubcosta@gmail.com>
2026-03-06 03:45:33 -0300

    Fechado session que ficava aberto em cada requisição

--- pucrio-dsv-full-stack/sprint-1/dsv_fullstack_basico/app_aula_1/app.py
+++ pucrio-dsv-full-stack/sprint-1/dsv_fullstack_basico/app_aula_1/app.py
@@ -35,60 +35,73 @@ def add_produto():
     except Exception as e:
         print(str(e))
         return jsonify({"message": error_msg[0]}), 400
+    finally:
+        session.close()
     
 @app.route('/get_produto/<produto_id>', methods=['GET'])
 def get_produto(produto_id):
     session = Session()
-    produto = session.query(Produto).filter(Produto.id == produto_id).first()
+    try:
+        produto = session.query(Produto).filter(Produto.id == produto_id).first()
 
-    if not produto:
-        return jsonify({"message": "Produto não encontrado!"}), 404
-    else:
-        return jsonify({"produto": produto.to_dict()})
+        if not produto:
+            return jsonify({"message": "Produto não encontrado!"}), 404
+        else:
+            return jsonify({"produto": produto.to_dict()})
+    finally:
+        session.close()
     
 @app.route('/get_produtos', methods=['GET'])
 def get_produtos():
     session = Session()
+    try:
+        lista_de_produto = [p.to_dict() for p in session.query(Produto).all()]
 
-    lista_de_produto = [p.to_dict() for p in session.query(Produto).all()]
-
-    if lista_de_produto:
-        output = {
-            "produtos": lista_de_produto,
-            "total": len(lista_de_produto)
-        }
-        return jsonify(output)
-    else:
-        return jsonify({"message": "Nenhum produto encontrado!"}), 404
+        if lista_de_produto:
+            output = {
+                "produtos": lista_de_produto,
+                "total": len(lista_de_produto)
+            }
+            return jsonify(output)
+        else:
+            return jsonify({"message": "Nenhum produto encontrado!"}), 404
+    finally:
+        session.close()
     
 @app.route('/remover_produto/<produto_id>', methods=['DELETE'])
 def remover_produto(produto_id):
     session = Session()
-    count = session.query(Produto).filter(Produto.id == produto_id).delete()
-    session.commit()
+    try:
+        count = session.query(Produto).filter(Produto.id == produto_id).delete()
+        session.commit()
 
-    if count == 1:
-        return jsonify({"message": "Produto removido!"})
-    else:
-        return jsonify({"message": "Produto não encontrado!"}), 404
+        if count == 1:
+            return jsonify({"message": "Produto removido!"})
+        else:
+            return jsonify({"message": "Produto não encontrado!"}), 404
+    finally:
+        session.close()
     
 @app.route('/add_comentario/<produto_id>', methods=['POST'])
 def add_comentario(produto_id):
     session = Session()
-    produto = session.query(Produto).filter(Produto.id == produto_id).first()
+    try:
+        produto = session.query(Produto).filter(Produto.id == produto_id).first()
 
-    if not produto:
-        return jsonify({"message": "Produto não encontrado!"}), 404
-    else:
-        data = request.get_json()
-        autor = data.get('autor')
-        texto = data.get('texto')
-        n_estrelas = data.get('n_estrela')
-        if n_estrelas:
-            n_estrelas = int(n_estrelas)
-        
-        comentario = Comentario(autor=autor, texto=texto, n_estrela=n_estrelas)
-        produto.adiciona_comentario(comentario=comentario)
-        session.commit()
+        if not produto:
+            return jsonify({"message": "Produto não encontrado!"}), 404
+        else:
+            data = request.get_json()
+            autor = data.get('autor')
+            texto = data.get('texto')
+            n_estrelas = data.get('n_estrela')
+            if n_estrelas:
+                n_estrelas = int(n_estrelas)
+            
+            comentario = Comentario(autor=autor, texto=texto, n_estrela=n_estrelas)
+            produto.adiciona_comentario(comentario=comentario)
+            session.commit()
 
-        return jsonify({"message": "Comentário adicionado!"})
+            return jsonify({"message": "Comentário adicionado!"})
+    finally:
+        session.close()
